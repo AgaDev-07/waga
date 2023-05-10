087ab8eee1299dddf15135c258ec3342b7698cfc
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-10 13:29:16 -0600

    export d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,19 @@
-export {};
+import Router from './lib/router';
+import { AgaRequest, AgaResponse } from './types';
+declare class App extends Router {
+    constructor();
+    listen(port: number, callback?: () => void): void;
+    toFunction(): (req: AgaRequest, res: AgaResponse) => Promise<void>;
+}
+declare function app(): App;
+declare namespace app {
+    export var json: () => (req: any, res: any, next: any) => void;
+    var _a: (path: any) => (req: any, res: any, next: any) => void;
+    export var cors: (options?: {
+        origin?: (origin: string, callback?: (err: Error, allow?: boolean) => void) => boolean;
+        methods?: string;
+    }) => (req: any, res: any, next: any) => void;
+    export var Router: () => Router;
+    export { _a as static };
+}
+export = app;
--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,6 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const http_1 = __importDefault(require("http"));
 const fs_1 = __importDefault(require("fs"));
 const fs_2 = __importDefault(require("@agacraft/fs"));
