5496e6558188571d88d2cea9cefd1f3db90588dd
AdrianCraft <88064157+AdrianCraft07@users.noreply.github.com>
2023-05-26 00:03:08 -0600

    1.5.1

--- src/lib/validatePath.ts
+++ src/lib/validatePath.ts
@@ -1,32 +1,31 @@
 export type ValidatePathFn = (pathname: string) => Record<string, string> | null;
 
-export default function validatePath (template:string):ValidatePathFn {
-		const pattern = template
-			.replace(/:[^/]+/g, '([^/]+)')
-			.replace(/\*/g, '(.*)');
-	
-		const regex = new RegExp(`^${pattern}$`);
-	
-		return (path) => {
-			const match = path.match(regex);
-	
-			if (match) {
-				const params = {};
-	
-				const paramNames = template.match(/:[^/]+/g) || [];
-				paramNames.forEach((paramName, index) => {
-					paramName = paramName.slice(1); // Remove the ":" from the paramName
-					params[paramName] = match[index + 1];
-				});
-	
-				if (template.includes('*')) {
-					const remainingPath = match[paramNames.length + 1];
-					params['*'] = remainingPath;
-				}
-	
-				return params;
+export default function validatePath(template: string): ValidatePathFn {
+	const pattern = template.replace(/:[^/]+/g, '([^/]+)').replace(/\*/g, '(.*)');
+
+	const regex = new RegExp(`^${pattern}$`);
+
+	return path => {
+		const match = path.match(regex);
+
+		if (match) {
+			const params = {};
+
+			const paramNames = template.match(/:[^/]+/g) || [];
+			paramNames.forEach((paramName, index) => {
+				paramName = paramName.slice(1); // Remove the ":" from the paramName
+				params[paramName] = match[index + 1];
+			});
+
+			if (template.includes('*')) {
+				const remainingPath = match[paramNames.length + 1];
+				if (remainingPath.startsWith('/')) params['*'] = remainingPath.slice(1);
+				else params['*'] = remainingPath;
 			}
-	
-			return null;
-		};
+
+			return params;
+		}
+
+		return null;
 	};
+}
